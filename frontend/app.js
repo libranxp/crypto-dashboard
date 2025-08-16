8bed8b70224518ab31c43d7d0165fa0d25c08f76
libranxp <130633300+libranxp@users.noreply.github.com>
2025-08-16 17:52:07 +0100

    app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,1 @@
-// Full implementation from previous response
-// Make sure to update the data URL to:
 const DATA_URL = 'https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/backend/scan_results.json';
